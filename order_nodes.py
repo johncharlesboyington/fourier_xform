46313eb75d525263387114cc75152e5a657db952
johncharlesboyington <johncharlesboyington@gmail.com>
2021-11-10 17:04:25 -0600

    few style fixes to order_nodes; reran maple.gif

--- order_nodes.py
+++ order_nodes.py
@@ -6,28 +6,33 @@ from node_finder import node_finder
 from spin_to_win import spin
 
 
-def distance(a, b):
-    return np.sqrt(np.sum((a - b)**2))
-
-
 def order_nodes(nodes, connections, image_name):
     """order_nodes"""
 
-    # containers
+    # containers used in the algorithm
     node_indices = list(range(len(nodes)))
     final_path = []
     node_list = list(np.array(node_indices))
+
+    # set the current node to the first node in the list
+    # this is our starting point
     current_node = node_list[0]
+
+    # removes the first node from the list of
+    # available nodes and places it in the path
     node_list.remove(current_node)
     final_path.append(current_node)
 
     # main loop
+    # the limit of 1000 iterations guarantees that you never get stuck in
+    # an infinite loop, and will instead error out
     for i in range(1000):
 
+        # if there are no more nodes in the list, we're done
         if not node_list:
             break
-        
-        # select next
+
+        # given a node, this pulls all of the 
         options = connections[current_node]
 
         # if at least one option is in node list
@@ -54,12 +59,12 @@ def order_nodes(nodes, connections, image_name):
     final_ordinates = []
     for node_index in final_path:
         final_ordinates.append(nodes[node_index])
-    
+
     # plotting
     fig = plt.figure(0)
     ax = fig.add_subplot(111)
     ax.plot(*np.array(final_ordinates).T)
-    
+
     fig.savefig('image/' + image_name + '_path.png')
     fig.clf()
     return final_ordinates
